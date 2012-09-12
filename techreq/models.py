5abd712e4af3f2b591c739ca1ac15ab64af2ed8c
Kyle Demeule <kdd2@sfu.ca>
2012-09-11 22:29:15 -0700

    Changing tabs to spaces for my previous few commits

--- techreq/models.py
+++ techreq/models.py
@@ -2,25 +2,25 @@ from django.db import models
 from coredata.models import CourseOffering, Unit
 
 class TechResource(models.Model):
-	name = models.CharField(max_length=60, help_text='Name of the resource.')
-	unit = models.ForeignKey(Unit)
-	version = models.CharField(max_length=30, null=True, blank=True, help_text='Version of the resource, if applicable.')
-	quantity = models.PositiveIntegerField(null=True, blank=True, help_text='Quantity of the resource, if applicable.')
-	# Meeting times use CharFields for room/location so I'm copying that for now, 
-	# we may need a more solid type for better requirement/resource location matching
-	location = models.CharField(max_length=20, help_text='Location of the resource.')
-	notes = models.TextField(null=True, blank=True, help_text='Any additional notes about the resource.')
+    name = models.CharField(max_length=60, help_text='Name of the resource.')
+    unit = models.ForeignKey(Unit)
+    version = models.CharField(max_length=30, null=True, blank=True, help_text='Version of the resource, if applicable.')
+    quantity = models.PositiveIntegerField(null=True, blank=True, help_text='Quantity of the resource, if applicable.')
+    # Meeting times use CharFields for room/location so I'm copying that for now, 
+    # we may need a more solid type for better requirement/resource location matching
+    location = models.CharField(max_length=20, help_text='Location of the resource.')
+    notes = models.TextField(null=True, blank=True, help_text='Any additional notes about the resource.')
 
-	def __unicode__(self):
-		return self.name
+    def __unicode__(self):
+        return self.name
 
 class TechRequirement(models.Model):
-	name = models.CharField(max_length=60, help_text='Name of the requirement.')
-	course_offering = models.ForeignKey(CourseOffering)
-	version = models.CharField(max_length=30, null=True, blank=True, help_text='Version of the requirement, if applicable.')
-	quantity = models.PositiveIntegerField(null=True, blank=True, help_text='Quantity of the requirement, if applicable.')
-	# Meeting times use CharFields for room/location so I'm copying that for now, 
-	# we may need a more solid type for better requirement/resource location matching
-	location = models.CharField(max_length=20, help_text='Location where the requirement is required.')
-	notes = models.TextField(null=True, blank=True, help_text='Any additional notes about the requirement.')
-	satisfied_by = models.ForeignKey(TechResource, null=True, blank=True, help_text='The resource that satisfies this requirement.')
+    name = models.CharField(max_length=60, help_text='Name of the requirement.')
+    course_offering = models.ForeignKey(CourseOffering)
+    version = models.CharField(max_length=30, null=True, blank=True, help_text='Version of the requirement, if applicable.')
+    quantity = models.PositiveIntegerField(null=True, blank=True, help_text='Quantity of the requirement, if applicable.')
+    # Meeting times use CharFields for room/location so I'm copying that for now, 
+    # we may need a more solid type for better requirement/resource location matching
+    location = models.CharField(max_length=20, help_text='Location where the requirement is required.')
+    notes = models.TextField(null=True, blank=True, help_text='Any additional notes about the requirement.')
+    satisfied_by = models.ForeignKey(TechResource, null=True, blank=True, help_text='The resource that satisfies this requirement.')
