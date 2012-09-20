770a2c6bf297bf207b379722e0f59d758033eb9b
kks27 <kks27@sfu.ca>
2012-09-20 12:31:48 -0700

    Fetched unit data and shown in drop down- Resources

--- techreq/forms.py
+++ techreq/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from techreq.models import TechResource, TechRequirement
-
+from coredata.models import Unit
 class TechReqForm(forms.Form):
     name = forms.CharField(required=True, label="Name", max_length=60, 
         help_text="Name of the tech requirement for this course offering.",
@@ -23,16 +23,16 @@ class TechReqForm(forms.Form):
 
 
 
-MY_CHOICES = (
-    ('1', 'Option 1'),
-    ('2', 'Option 2'),
-    ('3', 'Option 3'),
-)
+#MY_CHOICES = (
+ #   ('1', 'Option 1'),
+  #  ('2', 'Option 2'),
+   # ('3', 'Option 3'),
+#)
 
 class TechResourceForm(forms.Form):
 
     name = forms.CharField(required=True, label="Resource Name", max_length= 60)
-    unit = forms.ChoiceField(choices= MY_CHOICES)
+    unit = forms.ModelChoiceField(queryset=Unit.objects.all(),label="Unit is")
     version = forms.CharField(required=False, label="Version", max_length=30) 
     quantity = forms.IntegerField(required=False, label="Quantity")
     location =  forms.CharField(required=False, label="Location", max_length=20)
@@ -42,5 +42,5 @@ class TechResourceForm(forms.Form):
 
     def __init__(self, *args, **kwargs):
         super(TechResourceForm, self).__init__(*args, **kwargs)
-        self.unit = forms.ChoiceField(choices=MY_CHOICES)
+        self.unit = forms.ModelChoiceField(queryset=Unit.objects.all())
 
