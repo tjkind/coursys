a729e740e5f7dcf52822e64ebde7f530c6f9c2c1
kks27 <kks27@sfu.ca>
2012-09-24 14:24:19 -0700

    added edit url

--- urls.py
+++ urls.py
@@ -119,6 +119,8 @@ urlpatterns += patterns('',
     url(r'^techstaff/techrequirements/$', 'techreq.views.techstaff_manage_techreqs'),
     url(r'^techstaff/techrequirements/(?P<filter_type>\w+)$', 'techreq.views.techstaff_manage_techreqs'),
     url(r'^techstaff/techrequirement/satisfy/(?P<techreq_id>' + ID_RE + ')$', 'techreq.views.satisfy_techreq'),
+    url(r'^techstaff/techresources/edit/(?P<techresource_id>' + ID_RE + ')$', 'techreq.views.edit_techresources'),
+
 
     # course groups
 
