a997c1722bfefca188eab7997dde8476dd08e29a
kks27 <kks27@sfu.ca>
2012-10-03 22:44:15 -0700

    Test for TechResource Component ----one thing missing---

--- techreq/tests.py
+++ techreq/tests.py
@@ -1,4 +1,4 @@
-from coredata.models import Semester, CourseOffering, Person, Member
+from coredata.models import Semester, CourseOffering, Person, Member, Unit, Role
 from coredata.tests import create_offering
 from courselib.testing import basic_page_tests
 
@@ -97,3 +97,105 @@ class ApplicationTest(TestCase):
         # and it's alternate name is not(avoid false positives)
         self.assertContains(response, '<td>%s</td>' % (techreq_changed_name))
         self.assertNotContains(response, '<td>%s</td>' % (techreq_orig_name))
+
+
+
+
+class ApplicationTechResourceTest(TestCase):
+    fixtures = ['test_data']
+    #def setUp(self):
+    def test_application(self):
+        tech = Person.objects.get(userid="dzhao")
+        tech.save()
+
+        # add a tech resource
+        techres1 = TechResource(name="Visual Studio", unit=Unit.objects[1], location="CSIL Windows")
+        techres1.save()
+
+        # login the techstaff
+        client = Client()
+        client.login(ticket=tech.userid, service=CAS_SERVER_URL)  
+
+        # get the tech requirement managing page
+        url = reverse('techresources.views.manage_techresources')
+        response = basic_page_tests(self, client, url)
+        self.assertEqual(response.status_code, 200)
+
+        # check that our manually added tech requirement is in the page
+        self.assertContains(response, '<td>%s</td>' % (techres1.name))
+
+
+
+        # delete the requirement and check that the requirement is no longer on the page
+        post_data = {
+            'techresource_id':techres1.id,
+            'action':'del',
+        }
+
+        response = client.post(url, post_data, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertNotContains(response, '<td>%s</td>' % (techres1.name))
+
+
+        # add a requirement and check to see if you can find it on the new page
+        post_data = {
+            'name':'Eclipse',
+            'location':'anywhere',
+            'action':'add',
+        }
+
+        response = client.post(url, post_data, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, '<td>Eclipse</td>')
+
+        # add a second tech req to test the edit page
+        techresource_orig_name = "Python"
+        techresource_changed_name = "Python_alt"
+        techres2 = TechResource(name=techresource_orig_name,  location="CSIL Linux")
+        techres2.save()
+
+        # get the tech requirement managing page
+
+        url = reverse('techresources.views.manage_techresources')
+        response = basic_page_tests(self, client, url)
+        self.assertEqual(response.status_code, 200)
+
+        # check that our manually added tech requirement is in the page, 
+        # and it's alternate name is not(avoid false positives)
+        self.assertContains(response, '<td>%s</td>' % (techresources_orig_name))
+        self.assertNotContains(response, '<td>%s</td>' % (techresources_changed_name))
+
+
+
+        # get the edit tech requirement page
+        url = reverse('techreq.views.edit_techresources', kwargs={'techresource_id':techres2.id})
+        response = basic_page_tests(self, client, url)
+
+        self.assertEqual(response.status_code, 200)
+        # check that it has an input filled out with our techres name
+        self.assertContains(response, '<input name="name" value="%s"' % (techresources_orig_name))
+        self.assertNotContains(response, '<input name="name" value="%s"' % (techresources_changed_name))
+
+
+
+        # edit the requirement and check that the requirement is changed on the edit page and the manage tech req page
+        post_data = {
+            'techresource_id':techres2.id,
+            'action':'edit',
+            'name': techres_changed_name,
+            'location': techres2.location,
+            'quantity': "",
+            'version': "",
+            'notes': "",
+        }
+        response = client.post(url, post_data, follow=True)
+        self.assertEqual(response.status_code, 200)
+
+        # check that our manually added tech techresource is in the page, 
+        self.assertContains(response, '<td>%s</td>' % (techreq_changed_name))
+        self.assertNotContains(response, '<td>%s</td>' % (techreq_orig_name))
+
+
+
+
+
