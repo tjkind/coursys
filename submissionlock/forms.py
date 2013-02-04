a6a17305ebfc81c9419791f1eb4094b5dcda88f2
Ivan Ma <ivanhma88@gmail.com>
2013-02-03 20:39:12 -0800

    Modifed forms.py and views.py to try and utilize Model Formset in view all submissionlocks module. Previous code commented out in order to preserve easy revert. Added test.html.

--- submissionlock/forms.py
+++ submissionlock/forms.py
@@ -1,10 +1,19 @@
 from django import forms
+from django.forms.fields import CharField
+from django.forms import ModelForm
 import datetime
 
 from grades.models import Activity
 from coredata.models import Member
 from submissionlock.models import SubmissionLock 
 
-class SubmissionLockForm(forms.Form):
-    def __init__(self, *args, **kwargs):
-        super(SubmissionLockForm, self).__init__(*args, **kwargs)
+#class SubmissionLockForm(forms.Form):
+#    def __init__(self, *args, **kwargs):
+#        super(SubmissionLockForm, self).__init__(*args, **kwargs)
+#        for item in range(5):
+#            self.fields['test_field_%s' % item] = CharField(max_length=255)
+
+class SubmissionLockForm(ModelForm):
+    class Meta:
+        model = SubmissionLock
+        exclude = {'config'}
--- submissionlock/views.py
+++ submissionlock/views.py
@@ -5,18 +5,53 @@ from coredata.models import CourseOffering, Member
 from django.shortcuts import render, get_object_or_404
 from django.core.urlresolvers import reverse
 
+from submissionlock.forms import *
+from django.forms.models import modelformset_factory
+LockFormSet = modelformset_factory(SubmissionLock)
+
+#def submission_lock(request, course_slug, activity_slug):
+#    activity = Activity.objects.get(slug=activity_slug)
+#    course = get_object_or_404(CourseOffering, slug=course_slug)
+#    students = Member.objects.filter(offering=course, role="STUD").select_related('person', 'offering')
+#    locked_students = SubmissionLock.objects.filter(activity=activity).select_related('member', 'effective_date')
+#    context = {
+#        'students': students,
+#        'course_slug': course_slug,
+#        'activity_slug': activity_slug,
+#        'locked_students': locked_students,
+#    }
+#    return render(request, 'submissionlock/submission_lock.html', context)
+
 def submission_lock(request, course_slug, activity_slug):
     activity = Activity.objects.get(slug=activity_slug)
     course = get_object_or_404(CourseOffering, slug=course_slug)
     students = Member.objects.filter(offering=course, role="STUD").select_related('person', 'offering')
     locked_students = SubmissionLock.objects.filter(activity=activity).select_related('member', 'effective_date')
+
+    if request.method == 'POST':
+        #form = SubmissionLockForm(request.POST)
+        formset = LockFormSet(request.POST)
+        if form.isvalid():
+            #...
+            return HttpResponseRedirect(reverse('submissionlock.views.submission_lock'))
+    else:
+        #form = SubmissionLockForm()
+        formset = LockFormSet()
+    
+    course_activity_slug = course_slug + '/\+' + activity_slug
     context = {
         'students': students,
         'course_slug': course_slug,
         'activity_slug': activity_slug,
         'locked_students': locked_students,
-    }
-    return render(request, 'submissionlock/submission_lock.html', context)
+        'course_slug' : course_slug,
+        'activity_slug' : activity_slug,
+        'course_activity_slug' : course_activity_slug,
+        #'form' : form,
+        'formset' : formset,
+    }  
+    
+    return render(request, 'submissionlock/test.html', context)
 
 def _apply_lock(course, activity, lock_date):
     students = Member.objects.filter(offering=course, role="STUD").select_related('person', 'offering')
