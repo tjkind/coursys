745e9d2c5186531a7a1f47e9cdf624136c47be9c
Ivan Ma <ivanhma88@gmail.com>
2013-02-13 22:36:42 -0800

    Added additional form element for "hidden" field but that needs to be discussed further later. Added additional verification code in clean func. Form alignment still needs doing.

--- peerreview/forms.py
+++ peerreview/forms.py
@@ -2,24 +2,30 @@ from django import forms
 from django.db import models
 import datetime
 from coredata.models import Unit, Person, Role
+from django.utils.safestring import mark_safe
+from grades.forms import CustomSplitDateTimeWidget
 
 from peerreview.models import PeerReviewComponent
 
 class AddPeerReviewComponentForm(forms.Form):
-    #def __init__(self, *args, **kwargs):
-    due_date = forms.DateField(initial=datetime.date.today())
-    due_time = forms.TimeField(initial=datetime.time(0, 0, 0, 0))
-    number_reviews = forms.IntegerField()
+    #def __init__(self, class_size, *args, **kwargs):
+    #due_date = forms.DateField(initial=datetime.date.today())
+    #due_time = forms.TimeField(initial=datetime.time(0, 0, 0, 0))
+    due_date = forms.SplitDateTimeField(initial=datetime.datetime.now(), label=mark_safe('Deadline'), required=True,
+            help_text='Time format: HH:MM:SS, 24-hour time', widget=CustomSplitDateTimeWidget())
+    number_reviews = forms.IntegerField(initial=1, required=True)
+    active = forms.BooleanField(required=False)
     
     def clean(self):
         due_date = self.cleaned_data.get('due_date')
         number_reviews = self.cleaned_data.get('number_reviews')
-        if (due_date < datetime.date.today):
-            raise forms.ValidationError(u'Selected due date is before current datetime!')
+        active = self.cleaned_data.get('active')
+        if (due_date < datetime.datetime.today()):
+            raise forms.ValidationError(u'Selected due datetime is before current date!')
         if (number_reviews < 1):
             raise forms.ValidationError(u'Peer review must have at least 1 number reviews!')
-        #elif cleaned_data['number_reviews'] > class_size:
+        #if (number_reviews > class_size):
             #raise forms.ValidationError(u'Peer review number reviews cannot exceed class size!')
-        context = { 'due_date' : due_date, 'number_reviews' : number_reviews }
+        context = { 'due_date' : due_date, 'number_reviews' : number_reviews, 'active' : active }
         return context
     
--- peerreview/views.py
+++ peerreview/views.py
@@ -8,6 +8,7 @@ from courselib.auth import requires_role, HttpResponseRedirect, \
     ForbiddenResponse, requires_course_staff_by_slug
 from grades.models import Activity
 from peerreview.forms import AddPeerReviewComponentForm
+from peerreview.models import *
 
 
 @requires_course_staff_by_slug
@@ -16,8 +17,19 @@ def add_peer_review_component(request, course_slug, activity_slug):
     if request.method == 'POST':
         form = AddPeerReviewComponentForm(request.POST)
         if form.is_valid():
-            #Peer_Review_Component = form.save()
-            #do something
+            try: #see if peerreview component already exists for this activity
+                peerreview_component = peerreview.objects.get(activity=activity)
+                peerreview_component.due_date = form.cleaned_data['due_date']
+                peerreview_component.number_reviews = form.cleaned_data['number_reviews']
+                peerreview_component.hidden = form.cleaned_data['active'] #inversed at the moment, consider re-modeling!
+                peerreview_component.save()
+            except: #else create a peerreview component
+                peerreview_component = PeerReviewComponent.objects.create(
+                    activity = activity,
+                    due_date = form.cleaned_data['due_date'],
+                    number_reviews = form.cleaned_data['number_reviews'],
+                    hidden = form.cleaned_data['active'],
+                )
             return HttpResponseRedirect(reverse('grades.views.activity_info', kwargs={'course_slug': course_slug, 'activity_slug': activity_slug}))
     else:
         form = AddPeerReviewComponentForm()
